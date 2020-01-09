9d17d0af0b2384c62d2a83c245990fe4adbccf17
中山 陽介 <bp-ynakayama@kdl.co.jp>
2020-01-09 15:52:16 +0900

    Initial Commit

---
+++ CombineSwiftUISample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineSwiftUISample
+//
+//  Created by 中山 陽介 on 2020/01/09.
+//  Copyright © 2020 Yosuke Nakayama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
